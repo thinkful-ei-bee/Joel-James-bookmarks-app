42a0a922a3d4561e480b63221e3ca1733828668b
joelcorey <joelcorey@fastmail.com>
2019-03-05 12:53:03 -0800

    stub bookmark iife

---
+++ scripts/bookmark-list.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const bookmarkList = (function() {
+
+  // template functions here
+
+  
+
+  // renderer:
+  function render() {
+
+  }
+
+  function handleAddBookmark() {
+
+  }
+  
+  function handleMinRating() {
+  
+  }
+  
+  function handleSaveBookmark() {
+  
+  }
+  
+  function handleCancelBookmark() {
+  
+  }
+  
+  function handleDeleteBookmark() {
+  
+  }
+  
+  function handleExpandBookmark() {
+  
+  }
+  
+  function bindEventListeners() {
+    handleAddBookmark();
+    handleMinRating(); 
+    handleSaveBookmark();
+    handleCancelBookmark();
+    handleDeleteBookmark();
+    handleExpandBookmark();
+  }
+
+  return {
+    render,
+    bindEventListeners,
+  };
+
+}());
