6957ac27cba09a6d9f331ce2d83c1c4fe80dd1ee
joelcorey <joelcorey@fastmail.com>
2019-03-06 08:16:29 -0800

    document ready for testing

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,16 @@
 
 /* global bookmarkList, store, api */
 
-function main() {
+//function main() {
+$(document).ready(function() {
   bookmarkList.bindEventListeners();
 
   api.getBookmarks((bookmarks) => {
-    bookmarks.forEach((bookmark) => store.addBook(item));
+    bookmarks.forEach((bookmark) => store.addBook(bookmark));
     console.log(store.bookmarks);
   });
 
-}
+});
 
 
-$(main);
+//$(main);
