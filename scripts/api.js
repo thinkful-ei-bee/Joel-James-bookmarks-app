5ac984880288924511cbf5c1c912ecc50a34e65a
joelcorey <joelcorey@fastmail.com>
2019-03-06 07:58:05 -0800

    API stub

--- scripts/api.js
+++ scripts/api.js
@@ -1,12 +1,72 @@
 'use strict';
+
+/* global store */
+
 const api = (function(){
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/joel-james';
+  const baseUrl = 'https://thinkful-list-api.herokuapp.com/joel-james';
   
-  const getBookmark = function(){
-    $.getJSON(`${BASE_URL}/bookmarks`);
-  };
+  function getBookmarks() {
+    return bookmarkApiFetch(`${baseUrl}`);
+  }
+  
+  function createBookmark(name) {
+    return bookmarkApiFetch(`${baseUrl}/items`,
+      {
+        method: 'POST',
+        headers:
+          {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify({ name }),
+      });
+  }
+
+  function deleteBookmark(id) {
+    return bookmarkApiFetch(`${baseUrl}/items/${id}`, 
+      {
+        method: 'DELETE',
+      });
+  }
+
+  function updateBookmark(id, updateData) {
+    return bookmarkApiFetch(`${baseUrl}/items/${id}`,
+      {
+        method: 'PATCH',
+        headers:
+          {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(updateData),
+      });
+  } 
+
+
+  function bookmarkApiFetch(...args) {
+    let error;
+    return fetch(...args)
+      .then(res => {
+        if(!res.ok) {
+          error = { code: res.status };
+          StorageEvent.error = error;
+        }
+        return res.json();
+      })
+      .then(data => {
+
+        if (error) {
+          error.message = data.message;
+          return Promise.reject(error);
+        }
+
+        return data;
+      });
+  }
 
   return {
-      getBookmark , createBookmark, deleteBookmark 
-    };
+    getBookmarks,
+    createBookmark,
+    deleteBookmark,
+    updateBookmark,
+  };
+
 }());
